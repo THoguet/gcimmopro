e06194fac2400009650a877d6b5d1b7eea8d253b
Théophyl Hoguet <th.theophyl.hoguet@gmail.com>
2022-06-14 03:52:13 +0200

    update gcimmopro add container + image + text + responsve

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -1,7 +1,11 @@
 let lastKnownScrollPosition = 0;
 let ticking = false;
 
-document.addEventListener('scroll', function (e) {
-	lastKnownScrollPosition = window.scrollY;
-	console.log(window.scrollY);
+let container = document.getElementById("container");
+
+container.addEventListener('scroll', function (e) {
+	let lastElementIndex = container.children.length - 1;
+	if (container.children.item(lastElementIndex - 1).getBoundingClientRect().y == 0) {
+		console.log("avant dernier");
+	}
 });
